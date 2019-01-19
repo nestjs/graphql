56816b4d5fbe9d0a83d6866115bcd4ae65becd15
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-01-19 23:04:29 +0100

    bugfix() workaround - multiple graphql packages

--- lib/graphql-schema-builder.ts
+++ lib/graphql-schema-builder.ts
@@ -1,8 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { loadPackage } from '@nestjs/common/utils/load-package.util';
+import { readFile } from 'fs';
 import { GraphQLSchema } from 'graphql';
-import { addResolveFunctionsToSchema } from 'graphql-tools';
-import { MULTIPLE_GRAPHQL_ERROR } from './errors';
 import { BuildSchemaOptions } from './external/type-graphql.types';
 import { ScalarsExplorerService } from './services/scalars-explorer.service';
 
@@ -15,28 +14,21 @@ export class GraphQLSchemaBuilder {
   ) {}
 
   async build(
-    emitSchemaFile: string | boolean,
-    resolvers: any,
+    emitSchemaFile: string,
     options: BuildSchemaOptions = {},
-  ): Promise<any> {
-    try {
-      const buildSchema = this.loadBuildSchemaFactory();
-      const scalarsMap = this.scalarsExplorerService.getScalarsMap();
-      const schema = await buildSchema({
-        ...options,
-        emitSchemaFile,
-        scalarsMap,
-        resolvers: ['**/*.undefined.undefined.js'], // NOTE: Added to omit options validation
-      });
-      addResolveFunctionsToSchema({ schema, resolvers });
-      return schema;
-    } catch (err) {
-      this.logger.error(MULTIPLE_GRAPHQL_ERROR);
-      throw err;
-    }
+  ): Promise<string> {
+    const buildSchema = this.loadBuildSchemaFactory();
+    const scalarsMap = this.scalarsExplorerService.getScalarsMap();
+    await buildSchema({
+      ...options,
+      emitSchemaFile,
+      scalarsMap,
+      resolvers: ['**/*.undefined.undefined.js'], // NOTE: Added to omit options validation
+    });
     /**
      * Workaround
      * Ref: https://19majkel94.github.io/type-graphql/docs/faq.html#i-got-error-like-cannot-use-graphqlschema-object-object-from-another-module-or-realm-how-to-fix-that
+     */
     return await new Promise<string>((resolve, reject) =>
       readFile(
         emitSchemaFile,
@@ -44,7 +36,6 @@ export class GraphQLSchemaBuilder {
         (err, data) => (err ? reject(err) : resolve(data)),
       ),
     );
-    **/
   }
 
   private loadBuildSchemaFactory(): (...args: any[]) => GraphQLSchema {
