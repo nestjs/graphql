e7c05906be513bf83633a0c82c373eece448458e
Mnwa <michael@alfascience.org>
2019-06-17 01:21:34 +0300

    feat(subscription.decorator) added promise type to filter&resolver

--- lib/decorators/subscription.decorator.ts
+++ lib/decorators/subscription.decorator.ts
@@ -14,8 +14,17 @@ const { Subscription: TypeGqlSubscription } =
   optional('type-graphql') || ({} as any);
 
 export interface SubscriptionOptions {
-  filter?: (payload: any, variables: any, context: any) => boolean;
-  resolve?: (payload: any, args: any, context: any, info: any) => any;
+  filter?: (
+    payload: any,
+    variables: any,
+    context: any,
+  ) => boolean | Promise<boolean>;
+  resolve?: (
+    payload: any,
+    args: any,
+    context: any,
+    info: any,
+  ) => any | Promise<any>;
 }
 
 export function Subscription(): MethodDecorator;
