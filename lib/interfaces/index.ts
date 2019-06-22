7e235f9353e1ab3841d2be79751b83558bfdb30d
marcus-sa <marcus-sa@outlook.com>
2019-06-22 16:52:30 +0200

    feat(federation): interfaces for gateway module options

--- lib/interfaces/index.ts
+++ lib/interfaces/index.ts
@@ -1,5 +1,6 @@
 export * from './custom-scalar.interface';
 export * from './gql-exception-filter.interface';
+export * from './gql-gateway-module-options.interface';
 export {
   GqlModuleAsyncOptions,
   GqlModuleOptions,
