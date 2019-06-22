f2d5950f548966b00fc25fe869372f4e868b8c07
marcus-sa <marcus-sa@outlook.com>
2019-06-22 16:34:23 +0200

    feat(federation): ResolveReference decorator

--- lib/graphql.constants.ts
+++ lib/graphql.constants.ts
@@ -1,6 +1,7 @@
 export const RESOLVER_TYPE_METADATA = 'graphql:resolver_type';
 export const RESOLVER_NAME_METADATA = 'graphql:resolver_name';
 export const RESOLVER_PROPERTY_METADATA = 'graphql:resolve_property';
+export const RESOLVER_REFERENCE_METADATA = 'graphql:resolve_reference';
 export const RESOLVER_DELEGATE_METADATA = 'graphql:delegate_property';
 export const SCALAR_NAME_METADATA = 'graphql:scalar_name';
 export const SCALAR_TYPE_METADATA = 'graphql:scalar_type';
@@ -11,6 +12,8 @@ export const FIELD_TYPENAME = '__resolveType';
 export const GRAPHQL_MODULE_OPTIONS = 'GqlModuleOptions';
 export const GRAPHQL_MODULE_ID = 'GqlModuleId';
 export const SUBSCRIPTION_TYPE = 'Subscription';
+
+export const RESOLVER_REFERENCE_KEY = '__resolveReference';
 
 export const DEFINITIONS_FILE_HEADER = `
 /** ------------------------------------------------------
