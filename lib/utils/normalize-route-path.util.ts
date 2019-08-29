95872f115cb0b443d5c25069341780cb4d328980
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-08-29 12:28:19 +0200

    feat() normalize both paths

---
+++ lib/utils/normalize-route-path.util.ts
@@ -0,0 +1,18 @@
+function addStartingSlash(text: string) {
+  if (!text) {
+    return text;
+  }
+  return text[0] !== '/' ? '/' + text : text;
+}
+
+function stripEndingSlash(text: string) {
+  if (!text) {
+    return text;
+  }
+  return text[text.length - 1] === '/' ? text.slice(0, text.length - 1) : text;
+}
+
+export function normalizeRoutePath(path: string) {
+  path = addStartingSlash(path);
+  return stripEndingSlash(path);
+}
