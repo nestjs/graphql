bef82ab62e776bbf3b66e3072d48f570abee1b48
Rick Dutour Geerling <info@rickdg.nl>
2019-10-29 10:59:41 +0100

    test: add integration test for gateway module

---
+++ tests/graphql-federation/gateway/gateway.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { GraphQLGatewayModule } from '../../../lib/graphql-gateway.module';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLGatewayModule.forRoot({
+      serviceList: [
+        { name: 'users', url: 'http://localhost:3001/graphql' },
+        { name: 'posts', url: 'http://localhost:3002/graphql' },
+      ],
+    }),
+  ],
+})
+export class AppModule {}
