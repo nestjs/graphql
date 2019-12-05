c8ba59019ca27d6c6503fef254f5bfba43777e22
Rick Dutour Geerling <rick.geerling@jumbo.com>
2019-12-05 09:25:16 +0100

    fix: invert logic for providing your own schema

--- lib/graphql-federation.factory.ts
+++ lib/graphql-federation.factory.ts
@@ -33,7 +33,7 @@ export class GraphQLFederationFactory {
       // Enable support when Directive support in type-graphql goes stable
       throw new Error('Code-first not supported yet');
       schema = await this.generateSchema(options);
-    } else if (!isEmpty(options.typeDefs)) {
+    } else if (isEmpty(options.typeDefs)) {
       schema = options.schema;
     } else {
       schema = this.buildSchemaFromTypeDefs(options);
--- tests/e2e/typegraphql-federation.spec.ts
+++ tests/e2e/typegraphql-federation.spec.ts
@@ -1,14 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
-import { ApplicationModule } from '../type-graphql-federation/app.module';
+// import { ApplicationModule } from '../type-graphql-federation/app.module';
 
 describe.skip('TypeGraphQL - Federation', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      imports: [ApplicationModule],
+      imports: [], // ApplicationModule],
     }).compile();
 
     app = module.createNestApplication();
