a41d05bb1b9cb80163f6c8a3a82440fce633b273
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2020-04-06 01:04:16 +0900

    fix(): set argument type of parse-literal

--- lib/interfaces/custom-scalar.interface.ts
+++ lib/interfaces/custom-scalar.interface.ts
@@ -1,6 +1,8 @@
+import { ValueNode } from 'graphql';
+
 export interface CustomScalar<T, K> {
   description: string | null | undefined;
   parseValue(value: T): K | null | undefined;
   serialize(value: K): T | null | undefined;
-  parseLiteral(ast): K | null | undefined;
+  parseLiteral(ast: ValueNode): K | null | undefined;
 }
