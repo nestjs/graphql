0fe9f38e52a1a6f4405b8b995777948b68fc6efa
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2020-04-09 22:51:00 +0900

    fix(): custom-scalar.interface.ts

--- lib/interfaces/custom-scalar.interface.ts
+++ lib/interfaces/custom-scalar.interface.ts
@@ -1,8 +1,12 @@
-import { ValueNode } from 'graphql';
+import {
+  GraphQLScalarLiteralParser,
+  GraphQLScalarSerializer,
+  GraphQLScalarValueParser,
+} from 'graphql';
 
 export interface CustomScalar<T, K> {
   description: string | null | undefined;
-  parseValue(value: T): K | null | undefined;
-  serialize(value: K): T | null | undefined;
-  parseLiteral(ast: ValueNode): K | null | undefined;
+  parseValue: GraphQLScalarValueParser<K>;
+  serialize: GraphQLScalarSerializer<T>;
+  parseLiteral: GraphQLScalarLiteralParser<K>;
 }
