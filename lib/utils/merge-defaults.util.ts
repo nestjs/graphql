f89e205cbbaf67ee58ae144fb3667f153b970495
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-04-15 16:17:33 +0200

    feat(): use request context id symbol, attach to ctx

--- lib/utils/merge-defaults.util.ts
+++ lib/utils/merge-defaults.util.ts
@@ -1,3 +1,4 @@
+import { isFunction } from '@nestjs/common/utils/shared.utils';
 import { GqlModuleOptions } from '../interfaces/gql-module-options.interface';
 
 const defaultOptions: GqlModuleOptions = {
@@ -9,8 +10,34 @@ export function mergeDefaults(
   options: GqlModuleOptions,
   defaults: GqlModuleOptions = defaultOptions,
 ): GqlModuleOptions {
-  return {
+  const moduleOptions = {
     ...defaults,
     ...options,
   };
+  if (!moduleOptions.context) {
+    moduleOptions.context = ({ req }) => ({ req });
+  } else if (isFunction(moduleOptions.context)) {
+    moduleOptions.context = (...args: unknown[]) => {
+      const ctx = (options.context as Function)(...args);
+      const { req } = args[0] as Record<string, unknown>;
+      if (typeof ctx === 'object') {
+        return {
+          req,
+          ...ctx,
+        };
+      }
+      return ctx;
+    };
+  } else {
+    moduleOptions.context = ({ req }: Record<string, unknown>) => {
+      if (typeof options.context === 'object') {
+        return {
+          req,
+          ...options.context,
+        };
+      }
+      return options.context;
+    };
+  }
+  return moduleOptions;
 }
