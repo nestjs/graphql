876be364ad79dbc674ad90e0842954c925d87e50
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-05-04 15:19:18 +0200

    fix(): use schema directive visitor from graphql-tools

--- lib/federation/graphql-federation.module.ts
+++ lib/federation/graphql-federation.module.ts
@@ -10,7 +10,7 @@ import { loadPackage } from '@nestjs/common/utils/load-package.util';
 import { ApplicationConfig, HttpAdapterHost } from '@nestjs/core';
 import { MetadataScanner } from '@nestjs/core/metadata-scanner';
 import { ApolloServerBase } from 'apollo-server-core';
-import { SchemaDirectiveVisitor } from 'apollo-server';
+import { SchemaDirectiveVisitor } from 'graphql-tools';
 import { GraphQLAstExplorer } from '../graphql-ast.explorer';
 import { GraphQLSchemaBuilder } from '../graphql-schema.builder';
 import { GraphQLSchemaHost } from '../graphql-schema.host';
@@ -32,10 +32,10 @@ import {
   ScalarsExplorerService,
 } from '../services';
 import {
+  extend,
   generateString,
   mergeDefaults,
   normalizeRoutePath,
-  extend,
 } from '../utils';
 import { GraphQLFederationFactory } from './graphql-federation.factory';
 
