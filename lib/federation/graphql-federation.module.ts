b1189b38e77f98d05d6bd04a5d687f27eb584c82
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2020-05-04 15:13:44 +0200

    Update lib/federation/graphql-federation.module.ts

--- lib/federation/graphql-federation.module.ts
+++ lib/federation/graphql-federation.module.ts
@@ -10,7 +10,7 @@ import { loadPackage } from '@nestjs/common/utils/load-package.util';
 import { ApplicationConfig, HttpAdapterHost } from '@nestjs/core';
 import { MetadataScanner } from '@nestjs/core/metadata-scanner';
 import { ApolloServerBase } from 'apollo-server-core';
-import { SchemaDirectiveVisitor } from 'graphql-tools';
+import { SchemaDirectiveVisitor } from 'apollo-server';
 import { GraphQLAstExplorer } from '../graphql-ast.explorer';
 import { GraphQLSchemaBuilder } from '../graphql-schema.builder';
 import { GraphQLSchemaHost } from '../graphql-schema.host';
