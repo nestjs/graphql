135a396e9069e4f584d76ccfbdf41ed724d60314
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-06-23 15:28:07 +0200

    fix(): add enhancer type to the public api

--- lib/interfaces/index.ts
+++ lib/interfaces/index.ts
@@ -5,6 +5,7 @@ export * from './custom-scalar.interface';
 export * from './gql-exception-filter.interface';
 export * from './gql-gateway-module-options.interface';
 export {
+  Enhancer,
   GqlModuleAsyncOptions,
   GqlModuleOptions,
   GqlOptionsFactory,
