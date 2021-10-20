173df6594daa00741189b5a12a415b76c273d643
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2021-10-20 13:32:51 +0200

    Update lib/schema-builder/errors/cannot-determine-input-type.error.ts

--- lib/schema-builder/errors/cannot-determine-input-type.error.ts
+++ lib/schema-builder/errors/cannot-determine-input-type.error.ts
@@ -4,7 +4,7 @@ export class CannotDetermineInputTypeError extends Error {
   constructor(hostType: string, typeRef?: GqlTypeReference) {
     const inputObjectName: string | false = typeof typeRef === 'function' && typeRef.name;
     super(
-      `Cannot determine a GraphQL input type for the "${hostType}"${inputObjectName ? `, on "${inputObjectName}` : null}". Make sure your class is decorated with an appropriate decorator.`,
+      `Cannot determine a GraphQL input type ${inputObjectName ? `("${inputObjectName}") ` : null}for the "${hostType}". Make sure your class is decorated with an appropriate decorator.`,
     );
   }
 }
