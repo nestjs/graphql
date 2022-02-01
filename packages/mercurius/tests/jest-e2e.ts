6a8489204d0d3ca7665115400e443cb8ddd28318
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2022-02-01 12:06:52 +0100

    test(): replace ts-jest/utils import with just ts-jest

--- packages/mercurius/tests/jest-e2e.ts
+++ packages/mercurius/tests/jest-e2e.ts
@@ -1,5 +1,5 @@
 import type { Config } from '@jest/types';
-import { pathsToModuleNameMapper } from 'ts-jest/utils';
+import { pathsToModuleNameMapper } from 'ts-jest';
 import { compilerOptions } from '../tsconfig.spec.json';
 
 const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
