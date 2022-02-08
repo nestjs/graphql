39667eba846df29623748fcc24d3a5647a5eba5d
soudai-s <soudai.sasada@hotmail.com>
2022-02-08 18:50:26 +0900

    chore(graphql): expose boolean type as const named 'Boolean'

--- packages/graphql/lib/scalars/index.ts
+++ packages/graphql/lib/scalars/index.ts
@@ -1,4 +1,4 @@
-import { GraphQLFloat, GraphQLID, GraphQLInt } from 'graphql';
+import { GraphQLFloat, GraphQLID, GraphQLInt, GraphQLBoolean } from 'graphql';
 
 export * from './iso-date.scalar';
 export * from './timestamp.scalar';
@@ -6,3 +6,4 @@ export * from './timestamp.scalar';
 export const Int = GraphQLInt;
 export const Float = GraphQLFloat;
 export const ID = GraphQLID;
+export const Boolean = GraphQLBoolean;
