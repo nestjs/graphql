d60a167eb87f1297a688e1c937785a00f494c86a
Elies Lou <elies@gojob.com>
2022-02-10 14:08:09 +0100

    docs(apollo-federation): explain multigraph use-case

--- packages/apollo/tests/code-first-multigraph/main.ts
+++ packages/apollo/tests/code-first-multigraph/main.ts
@@ -3,6 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 import { GatewayModule } from './gateway.module';
 
+/**
+ * This use case comes from the need for an application
+ * to be designed in a way which would allow future context
+ * separation from a deployment point of view.
+ */
 export async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.useGlobalPipes(new ValidationPipe());
