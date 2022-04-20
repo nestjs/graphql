44877c30f34df7911a7c9d68741ce625979266f9
Charly POLY <cpoly55@gmail.com>
2022-04-20 17:01:55 +0200

    feat(gql-subscription): allow passing `execute` and `subscribe`

--- packages/graphql/lib/services/gql-subscription.service.ts
+++ packages/graphql/lib/services/gql-subscription.service.ts
@@ -1,4 +1,8 @@
-import { execute, GraphQLSchema, subscribe } from 'graphql';
+import {
+  execute as graphqlExecute,
+  GraphQLSchema,
+  subscribe as graphqlSubscribe,
+} from 'graphql';
 import { GRAPHQL_TRANSPORT_WS_PROTOCOL, ServerOptions } from 'graphql-ws';
 import { useServer } from 'graphql-ws/lib/use/ws';
 import {
@@ -38,6 +42,8 @@ export type SubscriptionConfig = {
 
 export interface GqlSubscriptionServiceOptions extends SubscriptionConfig {
   schema: GraphQLSchema;
+  execute?: typeof graphqlExecute;
+  subscribe?: typeof graphqlSubscribe;
   path?: string;
   context?: ServerOptions['context'];
 }
@@ -70,6 +76,8 @@ export class GqlSubscriptionService {
 
   private initialize() {
     const supportedProtocols = [];
+    const { execute = graphqlExecute, subscribe = graphqlSubscribe } =
+      this.options;
 
     if ('graphql-ws' in this.options) {
       const graphqlWsOptions =
