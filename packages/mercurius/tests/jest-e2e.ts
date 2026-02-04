c0450c3cb2b2316264771868db0a5ca336b3b407
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2026-02-04 12:45:58 +0100

    chore: update jest configuration files

--- packages/mercurius/tests/jest-e2e.ts
+++ packages/mercurius/tests/jest-e2e.ts
@@ -1,10 +1,13 @@
 import type { Config } from '@jest/types';
 import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from '../tsconfig.spec.json';
+import tsconfig from '../tsconfig.spec.json' with { type: 'json' };
 
-const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
-  prefix: '<rootDir>/',
-});
+const moduleNameMapper = pathsToModuleNameMapper(
+  tsconfig.compilerOptions.paths,
+  {
+    prefix: '<rootDir>/',
+  },
+);
 
 const config: Config.InitialOptions = {
   moduleFileExtensions: ['js', 'json', 'ts'],
