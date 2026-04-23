2cd85b478d0d7a1fec63d15d79f0245b175f40d2
mateuswgoettems <76021452+mateuswgoettems@users.noreply.github.com>
2026-04-22 23:53:05 -0300

    test(@nestjs/apollo): add e2e coverage for subscriptions with ApolloFederationDriver

---
+++ packages/apollo/tests/subscriptions/federation-app/notification.resolver.ts
@@ -0,0 +1,28 @@
+import { Args, Query, Resolver, Subscription } from '@nestjs/graphql';
+import { PubSub } from 'graphql-subscriptions';
+import { Notification } from './notification';
+
+export const pubSub = new PubSub();
+
+@Resolver(() => Notification)
+export class NotificationResolver {
+  @Query(() => Notification)
+  getFederatedNotification(): Notification {
+    return { id: '0', recipient: 'system', message: 'ok' };
+  }
+
+  @Subscription(() => Notification, {
+    filter(payload, variables) {
+      return (
+        payload.newFederatedNotification.id === variables.id &&
+        payload.newFederatedNotification.recipient === variables.recipient
+      );
+    },
+  })
+  newFederatedNotification(
+    @Args('id', { nullable: false }) id: string,
+    @Args('recipient', { nullable: false }) recipient: string,
+  ) {
+    return pubSub.asyncIterableIterator('newFederatedNotification');
+  }
+}
