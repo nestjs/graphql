feb8c6aa955122dc4f920b3d4e4bc4b71ad2eb01
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2026-09-22 20:21:39 +0200

    Revert "feat(@nestjs/graphql): add @BatchResolveField() for DataLoader-backed batching (#4123)" (#4148)
    This reverts commit 5f6518e6a2e73ccd22506dcca49b5382d3eff869.

--- packages/graphql/lib/interfaces/index.ts
+++ packages/graphql/lib/interfaces/index.ts
@@ -1,5 +1,4 @@
 export * from './base-type-options.interface.js';
-export * from './batch-loader-options.interface.js';
 export * from './build-schema-options.interface.js';
 export * from './complexity.interface.js';
 export * from './custom-scalar.interface.js';
